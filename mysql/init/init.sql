90ae93e4f21a8d930f97df0390a07fd972b45088
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2024-06-14 21:37:28 +0200

    Removing user creation mysql

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -1,7 +1,4 @@
 # create databases
 CREATE DATABASE IF NOT EXISTS `rootthebox` character set utf8mb4;
 
-# create rootthebox user and grant rights
-GRANT USER 'rootthebox'@'localhost' IDENTIFIED BY 'mysup3rs3cr3tpassw0rd';
-GRANT ALL PRIVILEGES ON rootthebox.* TO 'rootthebox'@'localhost';
 
